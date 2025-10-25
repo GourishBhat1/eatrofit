d08990ca2399ba3db640e1a44e59c3aeb70bc96e
GourishBhat <gyronotechgoa@gmail.com>
2025-10-25 16:23:45 +0530

    permission update

--- config/config.php
+++ config/config.php
@@ -1,9 +1,9 @@
 <?php
 // Database configuration
 return [
-    'DB_HOST' => getenv('DB_HOST') ?: 'localhost',
-    'DB_USER' => getenv('DB_USER') ?: 'root',
-    'DB_PASS' => getenv('DB_PASS') ?: '',
-    'DB_NAME' => getenv('DB_NAME') ?: 'eatrofit',
-    'BASE_URL' => getenv('BASE_URL') ?: 'http://localhost/eatrofit/public',
+    'DB_HOST' => getenv('DB_HOST') ?: '193.203.184.226',
+    'DB_USER' => getenv('DB_USER') ?: 'u929750551_eatrofit',
+    'DB_PASS' => getenv('DB_PASS') ?: 'Mx?OlKw2R/',
+    'DB_NAME' => getenv('DB_NAME') ?: 'u929750551_eatrofit',
+    // 'BASE_URL' => getenv('BASE_URL') ?: 'http://localhost/eatrofit/public',
 ];
